b7f82edf48120e1058611f5d10ad931bcff5f857
Gabriel Sanz Sztochryn <gabrielsanz2106@gmail.com>
2022-03-31 01:16:37 -0300

    Button ERROR

--- Events/Interaction/buttonInteraction.js
+++ Events/Interaction/buttonInteraction.js
@@ -9,6 +9,8 @@ module.exports = {
     if (!interaction.isButton()) return;
     const Button = client.buttons.get(interaction.customId);
 
+    if (!Button) return;
+
     if (
       Button.permission &&
       !interaction.member.permissions.has(Button.permission)
